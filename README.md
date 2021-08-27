a4de2ac015f443063283ce34fe829e95f952246f
Nicholas Spinner <spinnernicholas@gmail.com>
2021-08-27 15:50:54 -0700

    Added Benchmark Sample

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ This is a Visual Studio C# source generator for automatically generating enum ex
 
 You can thank Nick Shapsas on YouTube for making me aware of this. Check out his awesome video here: [C#'s Enum performance trap your code is suffering from](https://www.youtube.com/watch?v=BoE5Y6Xkm6w)
 
+## Benchmark
+```
+|                         Method |      Mean |     Error |    StdDev |  Gen 0 | Allocated |
+|------------------------------- |----------:|----------:|----------:|-------:|----------:|
+|                 NativeToString | 25.871 ns | 0.5217 ns | 0.4625 ns | 0.0057 |      24 B |
+|        SwitchStatementToString |  1.265 ns | 0.0512 ns | 0.0479 ns |      - |         - |
+|       SwitchExpressionToString |  1.399 ns | 0.0565 ns | 0.0529 ns |      - |         - |
+| EnumFastToStringDotNetToString |  1.510 ns | 0.0224 ns | 0.0175 ns |      - |         - |
+```
+
 ## Quick Start
 
 1. Add source generator reference to your project
---
+++ samples/Benchmark/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using EnumFastToStringGenerated;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchy>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class Benchy
+    {
+        [Benchmark]
+        public string NativeToString()
+        {
+            HumanStates state = HumanStates.Sleeping;
+            return state.ToString();
+        }
+
+        [Benchmark]
+        public string SwitchStatementToString()
+        {
+            return SwitchStatementToString(HumanStates.Sleeping);
+        }
+
+        [Benchmark]
+        public string SwitchExpressionToString()
+        {
+            return SwitchExpressionToString(HumanStates.Sleeping);
+        }
+
+        [Benchmark]
+        public string EnumFastToStringDotNetToString()
+        {
+            HumanStates state = HumanStates.Sleeping;
+            return state.FastToString();
+        }
+
+        public string SwitchStatementToString(HumanStates state)
+        {
+            switch (state)
+            {
+                case HumanStates.Idle:
+                    return nameof(HumanStates.Idle);
+                case HumanStates.Working:
+                    return nameof(HumanStates.Working);
+                case HumanStates.Sleeping:
+                    return nameof(HumanStates.Sleeping);
+                case HumanStates.Eating:
+                    return nameof(HumanStates.Eating);
+                case HumanStates.Dead:
+                    return nameof(HumanStates.Dead);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+        }
+
+        public string SwitchExpressionToString(HumanStates state)
+        {
+            return state switch
+            {
+                HumanStates.Idle => nameof(HumanStates.Idle),
+                HumanStates.Working => nameof(HumanStates.Working),
+                HumanStates.Sleeping => nameof(HumanStates.Sleeping),
+                HumanStates.Eating => nameof(HumanStates.Eating),
+                HumanStates.Dead => nameof(HumanStates.Dead),
+                _ => throw new ArgumentOutOfRangeException(nameof(state), state, null)
+            };
+        }
+    }
+
+    [FastToString]
+    public enum HumanStates
+    {
+        Idle,
+        Working,
+        Sleeping,
+        Eating,
+        Dead
+    }
+}
